bb3eff69359bdc70c130d37953734cad8176e31d
阿甘科技VIP <103356507+agkjVIP@users.noreply.github.com>
2022-04-22 07:57:14 +0800

    Update baidupan.js

--- baidupan.js
+++ baidupan.js
@@ -29,7 +29,7 @@ if ($response["body"]) {
                 "buy_time": 0,
                 "product_id": "1",
                 "auto_upgrade_to_svip": 0,
-                "end_time": 1672502399,
+                "end_time": 9999999999,
                 "cluster": "vip",
                 "detail_cluster": "svip",
                 "status": 0
