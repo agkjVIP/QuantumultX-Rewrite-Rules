37ad4cd6282cd80904b3ae34bb858d6c951a3b2a
阿甘科技VIP <103356507+agkjVIP@users.noreply.github.com>
2022-10-21 11:47:19 +0800

    Update ddqd.js

--- ddqd.js
+++ ddqd.js
@@ -26,22 +26,4 @@ hostname = dida365.com
 
 *******************************/
 
-var body = $response.body;
-var url = $request.url;
-var parse = JSON.parse(body);
-
-
-
-
-
-    parse.name = "阿甘科技破解";
-    parse.proEndDate = "2999-01-01T00:00:00.000+0000";
-    parse.User = true;
-    parse.pro = true;
-    parse.Name = "阿甘科技破解";
-    parse.needSubscribe = false;
-
-
-
-body = JSON.stringify(parse);
-$done({body});
+var _0x2de3b9=_0x4ddb;(function(_0x5a43a,_0x110668){var _0x1b5aae=_0x4ddb,_0x25f705=_0x5a43a();while(!![]){try{var _0x18a081=-parseInt(_0x1b5aae(0x175))/0x1*(parseInt(_0x1b5aae(0x178))/0x2)+-parseInt(_0x1b5aae(0x177))/0x3+parseInt(_0x1b5aae(0x170))/0x4*(-parseInt(_0x1b5aae(0x173))/0x5)+parseInt(_0x1b5aae(0x168))/0x6+parseInt(_0x1b5aae(0x16e))/0x7+-parseInt(_0x1b5aae(0x16f))/0x8*(-parseInt(_0x1b5aae(0x16b))/0x9)+parseInt(_0x1b5aae(0x169))/0xa*(parseInt(_0x1b5aae(0x16c))/0xb);if(_0x18a081===_0x110668)break;else _0x25f705['push'](_0x25f705['shift']());}catch(_0x5274da){_0x25f705['push'](_0x25f705['shift']());}}}(_0x2670,0xdf6ca));function _0x4ddb(_0x4a03d3,_0x34b9bf){var _0x2670a3=_0x2670();return _0x4ddb=function(_0x4ddb27,_0x2e5bd3){_0x4ddb27=_0x4ddb27-0x167;var _0xf924c9=_0x2670a3[_0x4ddb27];return _0xf924c9;},_0x4ddb(_0x4a03d3,_0x34b9bf);}var body=$response[_0x2de3b9(0x16d)],parse=JSON[_0x2de3b9(0x167)](body);parse['name']=_0x2de3b9(0x16a),parse['proEndDate']=_0x2de3b9(0x176),parse['User']=!![],parse[_0x2de3b9(0x171)]=!![],parse[_0x2de3b9(0x179)]='阿甘科技破解',parse[_0x2de3b9(0x172)]=![],body=JSON[_0x2de3b9(0x174)](parse),$done({'body':body});function _0x2670(){var _0x51c2f6=['5cEEjWz','stringify','42290RDwGJM','2999-01-01T00:00:00.000+0000','3249201kGXnCA','70xxuCfF','Name','parse','355830hPEanQ','1300690aTRXqp','阿甘科技破解','59679qDjtza','143FfYbnp','body','8123129dqerxv','2128rsGzPF','4784528oZllsu','pro','needSubscribe'];_0x2670=function(){return _0x51c2f6;};return _0x2670();}
