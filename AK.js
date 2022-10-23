c44062ac6b9f40592a192bcd186b45a43eefbbb1
阿甘科技VIP <103356507+agkjVIP@users.noreply.github.com>
2022-10-23 13:22:01 +0800

    Create AK.js

---
+++ AK.js
@@ -0,0 +1,26 @@
+/******************************	
+	
+	脚本作者：阿甘科技
+	更新时间：2022-10-17
+	问题反馈：qq:1565320199
+	QQ群：773554575
+	
+	******************************
+[rewrite_local]
+
+^https:\/\/tingyun\.info\/api\/(user\/(personal|getdevice)|Browse\/add) url script-response-body https://raw.githubusercontent.com/agkjVIP/QuantumultX-rules/main/AK.js
+
+[mitm] 
+
+hostname = tingyun.info
+*******************************/
+
+var body = $response.body;
+var parse = JSON.parse(body);
+parse.vip_time = "2099-01-01";
+parse.view_num = 999999;
+parse.vip = 1;
+parse.msg = "";
+parse.code = 200;
+body = JSON.stringify(parse);
+$done(body);
