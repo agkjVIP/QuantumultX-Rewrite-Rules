3880ba189d6a6a0d0c8ddbc944e7655ca2b3be8d
阿甘科技VIP <103356507+agkjVIP@users.noreply.github.com>
2022-10-24 08:10:21 +0800

    Update AK.js

--- AK.js
+++ AK.js
@@ -9,19 +9,10 @@
 
 [rewrite_local]
 
-http:\/\/tingyun\.info\/api\/(user\/(personal|getdevice)|Browse\/add) url script-response-body https://raw.githubusercontent.com/agkjVIP/QuantumultX-rules/main/AK.js
-
+#AK视频全解锁
+^http:\/\/tingyun\.info\/api\/community\/edit url response-body "code":\d+, response-body "code":200,
+^http:\/\/tingyun\.info\/api\/user\/personal url response-body .+ response-body {   "code": 200,   "msg": "个人信息",   "time": "1666534199",   "data": {     "id": 108267,     "group_id": 0,     "parentid": 0,     "area": "",     "imei": "",     "xqx": "",     "is_change": 0,     "hl": "",     "jyyx": "",     "guanzhu": 0,     "fensi": 0,     "number": "JSJYJE",     "photo": "http:\/\/yfui.club0",     "num_t": 0,     "num": "-1",     "t_number": "208",     "vip_time": "2099-09-08",     "integral": 0,     "power": 0,     "agent": 0,     "username": "Mango_444603",     "nickname": "Mango_444603",     "password": "6ec639b8718fdc65201c1ec0a6bc9308",     "salt": "l05aGW",     "email": "",     "mobile": "14725836920",     "avatar": "http:\/\/tupian.ds9rsqa.cn\/mrtx\/48_1588741687.jpg",     "level": 1,     "gender": 0,     "birthday": null,     "bio": "",     "money": "0.00",     "fencheng": 0,     "weichat": "客服QQ\/微信：928355547",     "yitian": "",     "yitianyuan": "",     "qitian": "",     "qitianyuan": "",     "shiwu": "",     "shiwuyuan": "",     "yiyue": "",     "yiyueyuan": "",     "sanyue": "",     "sanyueyuan": "",     "liuyue": "",     "liuyueyuan": "",     "yinian": "",     "yinianyuan": "",     "yongjiu": "",     "yongjiuyuan": "",     "yitianurl": "",     "qitianurl": "",     "shiwuurl": "",     "yiyueurl": "",     "sanyueurl": "",     "liuyueurl": "",     "yinianurl": "",     "yongjiuurl": "",     "url8": "",     "url9": "",     "score": 0,     "successions": 1,     "maxsuccessions": 1,     "prevtime": 1663603948,     "logintime": 1663603948,     "loginip": "120.235.69.55",     "loginfailure": 0,     "joinip": "120.235.69.55",     "jointime": 1663603948,     "createtime": 1663603948,     "updatetime": 1663603948,     "token": "",     "status": "normal",     "verification": "",     "age": 0,     "is_okami": 0,     "view_num": 0,     "is_verifyed": 0,     "v_num": 0,     "device_code": "acb1616e2f8e3130b32ff868773eb8a2",     "tj_user_count": 0,     "video": 0,     "fabu": 0,     "today_invite_number": 0,     "invite_number": 0,     "vip": 1,     "read": 0,     "prevtime_text": null,     "logintime_text": null,     "jointime_text": null,     "avatar_text": "http:\/\/tupian.ds9rsqa.cn\/mrtx\/48_1588741687.jpg"   } }
 
 [mitm] 
 hostname = tingyun.info
 *******************************/
-
-var body = $response.body;
-var parse = JSON.parse(body);
-parse.vip_time = "2099-01-01";
-parse.view_num = 999999;
-parse.vip = 1;
-parse.msg = "";
-parse.code = 200;
-body = JSON.stringify(parse);
-$done(body);
