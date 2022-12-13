2c11294eec5b0d6934d1bd3ca758bc3805e29bcb
阿甘科技VIP <103356507+agkjVIP@users.noreply.github.com>
2022-12-13 11:08:20 +0800

    Update 91SPCrack.js

--- 91SPCrack.js
+++ 91SPCrack.js
@@ -7,12 +7,13 @@
 更新时间：2022-12-12
 问题反馈：qq:1565320199
 QQ群：773554575
-
+数据来自：欧阳熙
 *******************************
 
 [rewrite_local]
 # > 91视频解锁会员
-^https?:\/\/.*\/m_sns\/(film|video|short_video)\/(film_detail|detail_list|apply_play) url request-header (\r\n)X-AUTH-TOKEN:.+(\r\n) request-header $1X-AUTH-TOKEN: eyJhbGciOiJIUzUxMiIsImlhdCI6MTY1ODU4NzYxMSwiZXhwIjoxNjc0MTM5NjExfQ.eyJpZCI6OTg1MTU4MDV9.fA63_1SLzMNKvknO3Mcz2K-swMbrEe4rBW7HtlpH0hFcexF_5zXs6B9quX0LMJTKJyR5y2zBkHskOEjhr-ME6Q$2
+^https?:\/\/.*\/m_sns\/(film|video|short_video)\/(film_detail|detail_list|apply_play) url request-header (\r\n)X-AUTH-TOKEN:.+(\r\n) request-header $1X-AUTH-TOKEN: eyJhbGciOiJIUzUxMiIsImlhdCI6MTY3MDU3Njg4MCwiZXhwIjoxNjg2MTI4ODgwfQ.eyJpZCI6NzQ5NDI5OTR9.JprriMU4EBYExWABbQU32bf7tDTwPCPm7gIJRzrDfto6J7oXXQ0U5PDfpNJWKKZTuIohgjX04Xhi84VwkKaWKg
+$2
 ^https?:\/\/.*\/(common\/splash_config|m_user\/(check_phone|behavior_verification_code)) url reject-200
 [mitm] 
 hostname = app.weilaixushi.com, api.orientlion.com, app.qdjdswkj.com, api.0954auto.com, appapi.xmblgg.com, api.*.*
