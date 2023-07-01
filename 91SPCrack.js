1947133daf33ac68c424cd2a753048717b254779
阿甘科技VIP <103356507+agkjVIP@users.noreply.github.com>
2023-07-01 13:08:23 +0800

    Update 91SPCrack.js
    can be used

--- 91SPCrack.js
+++ 91SPCrack.js
@@ -1,20 +1,20 @@
 /******************************
 
-脚本功能：91视频解锁VIP
-下载地址：https://app.fcbl.xyz/i-2XRJY9
+脚本功能：91视频解锁VIP无限看
+下载地址：https://app.phwbk.site/i-2XRJY9
 软件版本：*
 脚本作者：阿甘科技
-更新时间：2022-12-12
+更新时间：2023-7-1
 问题反馈：qq:1565320199
 QQ群：773554575
-数据来自：欧阳熙
+数据来自：阿甘科技
 
 *******************************
 
 [rewrite_local]
-^https?:\/\/.*\/m_user\/info url request-header (\r\n)X-AUTH-TOKEN:.+(\r\n) request-header $1X-AUTH-TOKEN: eyJhbGciOiJIUzUxMiIsImlhdCI6MTY3MDU3Njg4MCwiZXhwIjoxNjg2MTI4ODgwfQ.eyJpZCI6NzQ5NDI5OTR9.JprriMU4EBYExWABbQU32bf7tDTwPCPm7gIJRzrDfto6J7oXXQ0U5PDfpNJWKKZTuIohgjX04Xhi84VwkKaWKg
+^https?:\/\/.*\/m_user\/info url request-header (\r\n)X-AUTH-TOKEN:.+(\r\n) request-header $1X-AUTH-TOKEN: eyJhbGciOiJIUzUxMiIsImlhdCI6MTY4ODE4MjA2MCwiZXhwIjoxNzAzNzM0MDYwfQ.eyJpZCI6ODkyOTQ0NjF9.0kU5jqKwZ0y8RY0SkjaWceIkekunNJIlfKvSOiqx4_MW0KC2-YIo8W1SfcD_78qywXzxKr8f7lCqKC1qBtrbRg
 $2
-^https?:\/\/.*\/m_sns\/(film|video|short_video)\/(film_detail|detail_list|apply_play) url request-header (\r\n)X-AUTH-TOKEN:.+(\r\n) request-header $1X-AUTH-TOKEN: eyJhbGciOiJIUzUxMiIsImlhdCI6MTY3MDU3Njg4MCwiZXhwIjoxNjg2MTI4ODgwfQ.eyJpZCI6NzQ5NDI5OTR9.JprriMU4EBYExWABbQU32bf7tDTwPCPm7gIJRzrDfto6J7oXXQ0U5PDfpNJWKKZTuIohgjX04Xhi84VwkKaWKg
+^https?:\/\/.*\/m_sns\/(film|video|short_video)\/(film_detail|detail_list|apply_play) url request-header (\r\n)X-AUTH-TOKEN:.+(\r\n) request-header $1X-AUTH-TOKEN: eyJhbGciOiJIUzUxMiIsImlhdCI6MTY4ODE4MjA2MCwiZXhwIjoxNzAzNzM0MDYwfQ.eyJpZCI6ODkyOTQ0NjF9.0kU5jqKwZ0y8RY0SkjaWceIkekunNJIlfKvSOiqx4_MW0KC2-YIo8W1SfcD_78qywXzxKr8f7lCqKC1qBtrbRg
 $2
 ^https?:\/\/.*\/(common\/splash_config|m_user\/(check_phone|behavior_verification_code)) url reject-200
 
